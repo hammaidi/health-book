d6bba2be6c13d548aa6976a866a6d2f186976b18
Karim <karimhammaidi3@gmail.com>
2025-10-22 14:52:28 +0200

    Initial commit - Spring Boot project setup

---
+++ src/main/java/com/healthbook/HealthBookApplication.java
@@ -0,0 +1,13 @@
+package com.healthbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/healthbook/HealthBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healthbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
